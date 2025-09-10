d0230aff723a38b00e88543b3a504f1da01af894
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2025-09-10 16:40:19 +0200

    [Express Essentials] Adding mock

--- Node.js/express-essentials/index.js
+++ Node.js/express-essentials/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import data from "./data/mock.json" with {type: "json"}
 
 const app = express();
 
@@ -6,4 +7,5 @@ const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`The server is running on ${PORT}`);
+    console.log(data);
 })
