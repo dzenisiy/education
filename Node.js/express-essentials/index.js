5b825d3100917795af94e562c17844d5c5c7424e
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2025-09-11 13:55:27 +0200

    [Express Essentials] Adding routes

--- Node.js/express-essentials/index.js
+++ Node.js/express-essentials/index.js
@@ -5,6 +5,28 @@ const app = express();
 
 const PORT = 3000;
 
+//GET
+app.get("/", (request, response) => {
+    response.send("This is a GET request at /")
+})
+
+//POST
+app.post("/create", (request, response) => {
+    response.send("This is a POST request at /create")
+})
+
+//PUT
+app.put("/edit", (request, response) => {
+    response.send("This is a PUT request at /edit")
+})
+
+//DELETE
+app.delete("/delete", (request, response) => {
+    response.send("This is a DELETE request at /delete")
+})
+
+
+
 app.listen(PORT, () => {
     console.log(`The server is running on ${PORT}`);
     console.log(data);
