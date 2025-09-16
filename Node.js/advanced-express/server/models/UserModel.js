8d03a983ab59853d89c2b936e1c322d66c044512
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2025-09-16 12:46:26 +0200

    [Advanced Express] add bcrypt for password

--- Node.js/advanced-express/server/models/UserModel.js
+++ Node.js/advanced-express/server/models/UserModel.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const emailValidator = require('email-validator');
+const bcrypt = require('bcrypt');
+const { UserModel } = require('../../test/helper');
+
+const SALT_ROUNDS = 12;
 
 const UserSchema = mongoose.Schema({
   username: {
@@ -30,5 +34,22 @@ const UserSchema = mongoose.Schema({
 }, {
   timestamps: true,
 });
+
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  try {
+    const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+    user.password = hash;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
