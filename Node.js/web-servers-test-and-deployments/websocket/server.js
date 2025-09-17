014441e3ef97def3cd6e9d6bf372b8d616a993e5
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2025-09-17 19:16:07 +0200

    [Test and Deployment] implementing chat via sockets

--- Node.js/web-servers-test-and-deployments/websocket/server.js
+++ Node.js/web-servers-test-and-deployments/websocket/server.js
@@ -2,11 +2,30 @@ import {WebSocketServer} from "ws";
 
 const wss = new WebSocketServer({port: 3000});
 
-wss.on("connection", () => {
-  wss.on("close", () => {
+let messsages = [];
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(message.toString());
+    messsages.push(message.toString());
+    if (message.toString().toLowerCase() === "exit") {
+      ws.close();
+    } else {
+      wss.clients.forEach(client => client.send(message.toString()))
+    }
+  })
+  ws.on("close", () => {
     console.log("socket disconnected");
   });
   console.log("new socket connected");
+  ws.send("Welcome to live chat");
+
+  if (messsages.length) {
+    ws.send("Chat currently in session");
+    messsages.forEach((message) => {
+      ws.send(message.toString());
+    })
+  }
 });
 
 
