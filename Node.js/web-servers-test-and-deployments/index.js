7ddf1b4accabeedbced6812f09d00a088bd01fd2
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2025-09-17 16:09:48 +0200

    [Test and Deployment] making request with get method

--- Node.js/web-servers-test-and-deployments/index.js
+++ Node.js/web-servers-test-and-deployments/index.js
@@ -3,13 +3,6 @@ const fs = require('fs');
 
 const url = "https://www.w3schools.com/js/default.asp"
 
-const options = {
-  hostname: "www.w3schools.com",
-  port: 443,
-  path: "/js/default.asp",
-  method: "GET"
-};
-
 const request = https.get(url, (res) => {
   let download = fs.createWriteStream("js.html");
   console.log("response started");
